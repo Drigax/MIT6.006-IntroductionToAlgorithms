9d64d3c9b6ec366eae446498c5766c3d264d0cdc
Nicholas Barlow <nicklowbar@gmail.com>
2024-09-05 12:18:55 -0700

    finish implementing rest of array unit tests

--- lib/data_structures/test/test_resizable_array.cpp
+++ lib/data_structures/test/test_resizable_array.cpp
@@ -70,10 +70,8 @@ TEST(resizable_array_list_test, bracket_operator_indexes_properly)
     }
     
     unsigned long randIdx = static_cast<unsigned long>(rand()) % arr.count();
-    UNUSED(randIdx);
-    ASSERT_TRUE(true);
-    //arr[randIdx] = test_val;
-    //ASSERT_EQ(arr[randIdx], test_val);
+    arr[randIdx] = test_val;
+    ASSERT_EQ(arr[randIdx], test_val);
 }
 
 TEST(resizable_array_list_test, const_bracket_operator_indexes_properly)
@@ -95,43 +93,117 @@ TEST(resizable_array_list_test, const_bracket_operator_indexes_properly)
 TEST(resizable_array_list_test, get_indexes_properly)
 {
     resizable_array_list<int> arr;
-    ASSERT_NE(arr.capacity(), (unsigned long)0);
+    //TODO: this test relies on insert_last behavior, rather than some constructor.
+    for (unsigned long i = 0UL; i < arr.capacity(); ++i)
+    {
+        arr.insert_last(static_cast<int>(i));
+    }
+
+    unsigned long randIdx = (static_cast<unsigned long>(rand()) % arr.count());
+    ASSERT_EQ(static_cast<int>(randIdx), arr.get(randIdx));
+
 }
 
 TEST(resizable_array_list_test, get_returns_value_reference)
 {
+    //TODO: this test relies on insert_last behavior, rather than some constructor.
     resizable_array_list<int> arr;
-    ASSERT_NE(arr.capacity(), (unsigned long)0);
+    
+    for (unsigned long i = 0UL; i < arr.capacity(); ++i)
+    {
+        arr.insert_last(static_cast<int>(i));
+    }
+
+    unsigned long test_idx = (static_cast<unsigned long>(rand()) % arr.count());
+    int& reference = arr.get(test_idx);
+
+    int test_val = rand();
+    reference = test_val;
+
+    ASSERT_EQ(arr.get(test_idx), test_val);
 }
 
 TEST(resizable_array_list_test, set_indexes_properly)
 {
     resizable_array_list<int> arr;
-    ASSERT_NE(arr.capacity(), (unsigned long)0);
+
+    unsigned long test_idx = static_cast<unsigned long>(rand()) % arr.capacity();
+    int test_val = rand();
+
+    arr.set(test_idx, test_val);
+    ASSERT_EQ(arr.get(test_idx), test_val);
 }
 
 TEST(resizable_array_list_test, insert_at_indexes_and_shifts_properly)
 {
     resizable_array_list<int> arr;
-    ASSERT_NE(arr.capacity(), (unsigned long)0);
+
+    for (unsigned long i = 0UL; i < arr.capacity(); ++i)
+    {
+        arr.insert_last(static_cast<int>(i));
+    }
+
+    unsigned long test_idx = 2;
+    int test_val = rand();
+
+    arr.insert_at(test_idx, test_val);
+    ASSERT_EQ(arr.get(test_idx), test_val);
+
+    for(int i = test_idx + 1; static_cast<unsigned long>(i) < arr.count(); ++i)
+    {
+        ASSERT_EQ(arr.get(static_cast<unsigned long>(i)), i - 1);
+    }
 }
 
 TEST(resizable_array_list_test, delete_at_indexes_and_shifts_properly)
 {
     resizable_array_list<int> arr;
-    ASSERT_NE(arr.capacity(), (unsigned long)0);
+
+    for (unsigned long i = 0UL; i < arr.capacity(); ++i)
+    {
+        arr.insert_last(static_cast<int>(i));
+    }
+
+    unsigned long test_idx = 2;
+    ASSERT_LT(test_idx, arr.count());
+    arr.delete_at(test_idx);
+    
+    for(int i = test_idx; i < static_cast<int>(arr.count()); ++i)
+    {
+        ASSERT_EQ(arr.get(static_cast<unsigned long>(i)), i + 1);
+    }
 }
 
 TEST(resizable_array_list_test, insert_first_inserts_and_shifts_properly)
 {
     resizable_array_list<int> arr;
-    ASSERT_NE(arr.capacity(), (unsigned long)0);
+
+    for (unsigned long i = 0UL; i < arr.capacity(); ++i)
+    {
+        arr.insert_first(static_cast<int>(i));
+    }
+
+    for(int i = 0; i < static_cast<int>(arr.count()); ++i)
+    {
+        ASSERT_EQ(arr.get(static_cast<unsigned long>(i)), static_cast<int>(arr.count()) - i - 1);
+    }
 }
 
 TEST(resizable_array_list_test, delete_first_inserts_and_shifts_properly)
 {
     resizable_array_list<int> arr;
-    ASSERT_NE(arr.capacity(), (unsigned long)0);
+
+    for (unsigned long i = 0UL; i < arr.capacity(); ++i)
+    {
+        arr.insert_last(static_cast<int>(i));
+    }
+
+    arr.delete_first();
+    
+    for(int i = 0; i < static_cast<int>(arr.count()); ++i)
+    {
+        ASSERT_EQ(arr.get(static_cast<unsigned long>(i)), i + 1);
+    }
 }
 
 TEST(resizable_array_list_test, resizes_to_hold_more_elements)
